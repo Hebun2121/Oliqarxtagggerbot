32b42b804b5368b1b7eb262059f99d2625ecd80d
Hebun2121 <166052714+Hebun2121@users.noreply.github.com>
2024-04-16 17:04:21 +0300

    Update EtiketTagger.py

--- EtiketTagger.py
+++ EtiketTagger.py
@@ -95,9 +95,9 @@ async def start(event):
                       [
                        Button.url('🎉  beni gruba davet et  🎉', f'https://t.me/{USERNAME}?startgroup=a')],
                       [
-                       Button.url('📚  komutlar ', f'https://t.me/ekselansmalikane'), #komutların olduğu kanal
-                       Button.url('👨‍💻  Sahip  ', f'https://t.me/Dalton3421')], #sahibin telegram profil linki
-                       [Button.url('📝  Kanal  ', f'https://t.me/ekselansmalikane@Dalton342121991912')] #Kanalın destek grubu vs
+                       Button.url('📚  komutlar ', f'https://t.me/diyarbakirsohbet2121'), #komutların olduğu kanal
+                       Button.url('👨‍💻  Sahip  ', f'https://t.me/Dalton2134')], #sahibin telegram profil linki
+                       [Button.url('📝  Kanal  ', f'https://t.me/diyarbakirsohbet2121')] #Kanalın destek grubu vs
                     ),
                     link_preview=False)
 
@@ -113,9 +113,9 @@ async def handler(event):
      await event.edit(f"{ad} {startmesaj}", buttons=(
                       [
                        Button.url('🎉  beni gruba davet et 🎉', f'https://t.me/{USERNAME}?startgroup=a')],
-                      [Button.url("📚  komutlar ", f'https://t.me/ekselansmalikane'), #komutların olduğu kanal
-                       Button.url('👨‍💻  Sahib  ', f'https://t.me/Dalton3421')] #sahibin telegram profil linki
-                       [Button.url('📝  Kanal  ', f'https://t.me/ekselansmalikane')] #Kanalın destek grubu vs
+                      [Button.url("📚  komutlar ", f'https://t.me/diyarbakirsohbet2121'), #komutların olduğu kanal
+                       Button.url('👨‍💻  Sahib  ', f'https://t.me/Dalton2134')] #sahibin telegram profil linki
+                       [Button.url('📝  Kanal  ', f'https://t.me/diyarbakirsohbet2121')] #Kanalın destek grubu vs
                     ),
                     link_preview=False)
 
@@ -124,8 +124,8 @@ async def handler(event):
 async def handler(event):
     await event.edit(f"{komutlar}", buttons=(
                       [
-                      Button.url('📣 Sohbet grubu  ', f'https://t.me/ekselansmalikane'), #destek grubun linki
-                      Button.url('📣 SAHİP ', f'https://t.me/Dalton3421') #sahibin telegram profil linki
+                      Button.url('📣 Sohbet grubu  ', f'https://t.me/diyarbakirsohbet2121'), #destek grubun linki
+                      Button.url('📣 SAHİP ', f'https://t.me/Dalton2134') #sahibin telegram profil linki
                       ],
                       [
                       Button.inline("<  𝖦𝖾𝗋𝗂  >", data="start"),
@@ -163,7 +163,7 @@ async def mentionall(event):
     await client.send_message(event.chat_id, " 📢 üyeleri etiketleme işlemi başladı . . .",
                     buttons=(
                       [
-                      Button.url('📝  𝖪ANAL  📝', f'https://t.me/ekselansmalikane')
+                      Button.url('📝  𝖪ANAL  📝', f'https://t.me/diyarbakirsohbet2121')
                       ]
                     )
                   ) 
@@ -177,7 +177,7 @@ async def mentionall(event):
         await event.respond("⛔ üyeleri etiketleme işlemi durduruldu. . .",
                     buttons=(
                       [
-                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/ekselansmalikane')
+                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/diyarbakirsohbet2121')
                       ]
                     )
                   )
@@ -222,7 +222,7 @@ async def mentionalladmin(event):
     await client.send_message(event.chat_id, " 📢 𝖠𝖽𝗆𝗂𝗇 etiket islemi başladı . . .",
                     buttons=(
                       [
-                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/ekselansmalikane')
+                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/diyarbakirsohbet2121')
                       ]
                     )
                   ) 
@@ -236,7 +236,7 @@ async def mentionalladmin(event):
         await event.respond("⛔ 𝖠𝖽𝗆𝗂𝗇 etiket islemi durduruldu . . .",
                     buttons=(
                       [
-                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/ekselansmalikane')
+                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/diyarbakirsohbet2121')
                       ]
                     )
                   )
@@ -281,7 +281,7 @@ async def tektag(event):
     await client.send_message(event.chat_id, " 📢 üyeleri etiketleme işlemi başladı . . .",
                     buttons=(
                       [
-                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/ekselansmalikane')
+                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/diyarbakirsohbet2121')
                       ]
                     )
                   ) 
@@ -295,7 +295,7 @@ async def tektag(event):
         await event.respond("⛔ üyeleri etiketleme işlemi durduruldu . . .",
                     buttons=(
                       [
-                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/ekselansmalikane')
+                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/diyarbakirsohbet2121')
                       ]
                     )
                   )
@@ -355,7 +355,7 @@ async def etag(event):
     await client.send_message(event.chat_id, " 📢 üyeleri etiketleme işlemi başladı . . .",
                     buttons=(
                       [
-                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/ekselansmalikane')
+                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/diyarbakirsohbet2121')
                       ]
                     )
                   ) 
@@ -369,7 +369,7 @@ async def etag(event):
         await event.respond("⛔ üyeleri etiketleme işlemi durduruldu . . .",
                     buttons=(
                       [
-                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/ekselansmalikane')
+                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/diyarbakirsohbet2121')
                       ]
                     )
                   )
@@ -464,7 +464,7 @@ async def stag(event):
     await client.send_message(event.chat_id, " 📢 uyeleri etiketleme başladıldı . . .",
                     buttons=(
                       [
-                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/ekselansmalikane')
+                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/diyarbakirsohbet2121')
                       ]
                     )
                   ) 
@@ -478,7 +478,7 @@ async def stag(event):
         await event.respond("⛔ üyeleri etiketleme işlemi durduruldu . . .",
                     buttons=(
                       [
-                       Button.url('📝  KANAL  📝', f'https://t.me/ekselansmalikane')
+                       Button.url('📝  KANAL  📝', f'https://t.me/diyarbakirsohbet2121')
                       ]
                     )
                   )
@@ -530,7 +530,7 @@ async def rtag(event):
     await client.send_message(event.chat_id, "✅ üyeleri etiketleyin . . .",
                     buttons=(
                       [
-                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/ekselansmalikane')
+                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/diyarbakirsohbet2121')
                       ]
                     )
                   ) 
@@ -544,7 +544,7 @@ async def rtag(event):
         await event.respond("⛔ üyeleri etiketleme işlemi durduruldu ...",
                     buttons=(
                       [
-                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/ekselansmalikane')
+                       Button.url('📝  𝖪ANAL  📝', f'https://t.me/diyarbakirsohbet2121')
                       ]
                     )
                   )
@@ -582,6 +582,6 @@ async def eros_oku(event):
 client.run_until_disconnected()
 
 
-print(">> Bot çalışmaktadir merak etme 🚀 @Dalton3421 bilgi alabilirsin. <<")
+print(">> Bot çalışmaktadir merak etme 🚀 @Dalton2134 bilgi alabilirsin. <<")
 client.run_until_disconnected()
 run_until_disconnected()
